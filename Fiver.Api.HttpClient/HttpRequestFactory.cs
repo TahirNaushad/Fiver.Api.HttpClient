64a63df5935d93f7e4aa2765ba52cb3ee1d7b95b
Tahir Naushasd <t.naushad25@gmail.com>
2017-07-12 09:48:35 +0100

    added methods to factory that taken token

--- Fiver.Api.HttpClient/HttpRequestFactory.cs
+++ Fiver.Api.HttpClient/HttpRequestFactory.cs
@@ -6,60 +6,89 @@ namespace Fiver.Api.HttpClient
     public static class HttpRequestFactory
     {
         public static async Task<HttpResponseMessage> Get(string requestUri)
+            => await Get(requestUri, "");
+
+        public static async Task<HttpResponseMessage> Get(string requestUri, string bearerToken)
         {
             var builder = new HttpRequestBuilder()
                                 .AddMethod(HttpMethod.Get)
-                                .AddRequestUri(requestUri);
+                                .AddRequestUri(requestUri)
+                                .AddBearerToken(bearerToken);
 
             return await builder.SendAsync();
         }
 
         public static async Task<HttpResponseMessage> Post(string requestUri, object value)
+            => await Post(requestUri, value, "");
+
+        public static async Task<HttpResponseMessage> Post(
+            string requestUri, object value, string bearerToken)
         {
             var builder = new HttpRequestBuilder()
                                 .AddMethod(HttpMethod.Post)
                                 .AddRequestUri(requestUri)
-                                .AddContent(new JsonContent(value)) ;
+                                .AddContent(new JsonContent(value))
+                                .AddBearerToken(bearerToken);
 
             return await builder.SendAsync();
         }
 
         public static async Task<HttpResponseMessage> Put(string requestUri, object value)
+            => await Put(requestUri, value, "");
+
+        public static async Task<HttpResponseMessage> Put(
+            string requestUri, object value, string bearerToken)
         {
             var builder = new HttpRequestBuilder()
                                 .AddMethod(HttpMethod.Put)
                                 .AddRequestUri(requestUri)
-                                .AddContent(new JsonContent(value));
+                                .AddContent(new JsonContent(value))
+                                .AddBearerToken(bearerToken);
 
             return await builder.SendAsync();
         }
 
         public static async Task<HttpResponseMessage> Patch(string requestUri, object value)
+            => await Patch(requestUri, value, "");
+
+        public static async Task<HttpResponseMessage> Patch(
+            string requestUri, object value, string bearerToken)
         {
             var builder = new HttpRequestBuilder()
                                 .AddMethod(new HttpMethod("PATCH"))
                                 .AddRequestUri(requestUri)
-                                .AddContent(new PatchContent(value));
+                                .AddContent(new PatchContent(value))
+                                .AddBearerToken(bearerToken);
 
             return await builder.SendAsync();
         }
 
         public static async Task<HttpResponseMessage> Delete(string requestUri)
+            => await Delete(requestUri, "");
+
+        public static async Task<HttpResponseMessage> Delete(
+            string requestUri, string bearerToken)
         {
             var builder = new HttpRequestBuilder()
                                 .AddMethod(HttpMethod.Delete)
-                                .AddRequestUri(requestUri);
+                                .AddRequestUri(requestUri)
+                                .AddBearerToken(bearerToken);
 
             return await builder.SendAsync();
         }
-        
+
         public static async Task<HttpResponseMessage> PostFile(string requestUri,
             string filePath, string apiParamName)
+            => await PostFile(requestUri, filePath, apiParamName, "");
+
+        public static async Task<HttpResponseMessage> PostFile(string requestUri,
+            string filePath, string apiParamName, string bearerToken)
         {
             var builder = new HttpRequestBuilder()
                                 .AddMethod(HttpMethod.Post)
                                 .AddRequestUri(requestUri)
-                                .AddContent(new FileContent(filePath, apiParamName));
+                                .AddContent(new FileContent(filePath, apiParamName))
+                                .AddBearerToken(bearerToken);
 
             return await builder.SendAsync();
         }
